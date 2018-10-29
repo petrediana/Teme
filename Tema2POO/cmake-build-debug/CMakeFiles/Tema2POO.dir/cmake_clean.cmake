08ac2986bdeec198fc8c0e1bf7d325f5b1d01ef5
Petre Diana <petre.diana98@yahoo.ro>
2018-10-29 10:21:52 +0200

    tema2

---
+++ Tema2POO/cmake-build-debug/CMakeFiles/Tema2POO.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Tema2POO.dir/main.cpp.obj"
+  "Tema2POO.pdb"
+  "Tema2POO.exe"
+  "Tema2POO.exe.manifest"
+  "libTema2POO.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Tema2POO.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
